63ac8c29bd28431ef63fc7ba67fe1018813651e4
reinaldow-dev <reinaldoweb@hotmail.com>
2022-06-05 14:07:37 -0300

    Adicionando model cliente e pedido

--- app/Cliente.php
+++ app/Cliente.php
@@ -3,12 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\Request;
 
 class Cliente extends Model
 {
 
     protected $fillable = ['nome','cpf','enderece','telefone','email'];
 
+    public function clienteInsert(Request $request){
+        $data = [
+            'nome' => $request->nome,
+            'cpf' => $request->cpf,
+            'endereco' => $request->endereco,
+            'telefone' => $request->telefone,
+            'email' =>$ $request->email
+        ];
+        Cliente::create($data);
+    }
 
     //
 }
--- app/Pedido.php
+++ app/Pedido.php
@@ -3,10 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\Request;
 
 class Pedido extends Model
 {
     protected $fillable = ['produto_id', 'cliente_id', 'cod_endereco'];
+
+    public function pedidoInsert(Request $request){
+
+        $data = [
+            'produto_id' => $request->produto_id,
+            'cliente_id' => $request->cliente_id,
+            'cod_endereco' => $request->cod_endereco,
+        ];
+        Pedido::create($data);
+    }
+
 
     public function produtos(){
 
